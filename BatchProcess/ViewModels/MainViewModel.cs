a04ea8d62d36af20d03d44b9c405a98f512520c6
fallenleavesbuy <9261562+fallenleavesguy@users.noreply.github.com>
2025-06-08 15:23:48 +0800

    finish video 3

---
+++ BatchProcess/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Svg.Skia;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BatchProcess.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private bool _sideMenuExpanded = false;
+
+    [RelayCommand]
+    private void SideMenuResize()
+    {
+        this.SideMenuExpanded = !this.SideMenuExpanded;
+    }
+}
