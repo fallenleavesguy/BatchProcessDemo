25a045361ff96bd208b264023b81a19ef611c4d4
fallenleavesbuy <9261562+fallenleavesguy@users.noreply.github.com>
2025-06-08 22:37:37 +0800

    finished video 6

--- BatchProcess/ViewModels/MainViewModel.cs
+++ BatchProcess/ViewModels/MainViewModel.cs
@@ -11,7 +11,6 @@ namespace BatchProcess.ViewModels;
 public partial class MainViewModel : ViewModelBase
 {
     private PageFactory _pageFactory;
-    private const string ButtonActiveClass = "active";
     
     [ObservableProperty] private bool _sideMenuExpanded = true;
 
@@ -32,6 +31,14 @@ public partial class MainViewModel : ViewModelBase
     public bool ReporterPageIsActive => CurrentPage.PageNames == ApplicationPageNames.Reporter;
     public bool HistoryPageIsActive => CurrentPage.PageNames == ApplicationPageNames.History;
     public bool SettingsPageIsActive => CurrentPage.PageNames == ApplicationPageNames.Settings;
+
+    /// <summary>
+    /// Design-time only constructor
+    /// </summary>
+    public MainViewModel()
+    {
+        CurrentPage = new SettingsPageViewModel();
+    }
 
     public MainViewModel(PageFactory pageFactory)
     {
--- BatchProcess/ViewModels/SettingsPageViewModel.cs
+++ BatchProcess/ViewModels/SettingsPageViewModel.cs
@@ -1,13 +1,26 @@
-﻿using BatchProcess.Data;
+﻿using System.Collections.Generic;
+using BatchProcess.Data;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace BatchProcess.ViewModels;
 
 public partial class SettingsPageViewModel: PageViewModel
 {
-    public string Test { get; set; } = "Process";
+    [ObservableProperty]
+    private List<string> _locationPaths;
     
     public SettingsPageViewModel()
     {
         PageNames = ApplicationPageNames.Settings;
+        
+        // TEMP: Remove
+        LocationPaths =
+        [
+            @"c:\temp\batchprocess\",
+            @"c:\temp\batchprocess\logs\",
+            @"c:\temp\batchprocess\reports\",
+            @"c:\temp\batchprocess\actions\",
+            @"c:\temp\batchprocess\macros\",
+        ];
     }
-}
+};
